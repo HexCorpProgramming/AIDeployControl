a2f84fcd5597e8a4e52d52ddb5e593e36bc93514
Drone #9813 <9813@hexcorp.net>
2020-08-24 21:12:25 +0200

    error feedback and disabling buttons while an operation is being handled

--- aideploycontrol/static/index.js
+++ aideploycontrol/static/index.js
@@ -1,7 +1,40 @@
+
+function setButtonsDisabled(disabled) {
+    document.querySelectorAll("button").forEach(element => {
+        element.disabled = disabled;
+    });
+}
+
+function setElementsHidden(selector, hidden) {
+    document.querySelectorAll(selector).forEach(element => {
+        if (hidden) {
+            element.classList.add("hidden");
+        } else {
+            element.classList.remove("hidden");
+        }
+    });
+}
+
 function launchAI() {
-    axios.post('/launch');
+    setButtonsDisabled(true);
+    setElementsHidden("#error-launch", true);
+    setElementsHidden("#waiting", false);
+    axios.post('/launch').then(Promise.resolve).catch(() => {
+        setElementsHidden("#error-launch", false);
+    }).finally(() => {
+        setButtonsDisabled(false)
+        setElementsHidden("#waiting", true);
+    });
 }
 
 function stopAI() {
-    axios.post('/stop');
+    setButtonsDisabled(true);
+    setElementsHidden("#error-stop", true);
+    setElementsHidden("#waiting", false);
+    axios.post('/stop').then(Promise.resolve).catch(() => {
+        setElementsHidden("#error-stop", false);
+    }).finally(() => {
+        setButtonsDisabled(false)
+        setElementsHidden("#waiting", true);
+    });
 }
