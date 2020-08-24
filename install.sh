6e2d40576658ecaa7a26c8792732047554a92dc2
Drone #9813 <9813@hexcorp.net>
2020-08-24 16:50:03 +0200

    initial commit

---
+++ install.sh
@@ -0,0 +1,12 @@
+#add nice alias for sourcing
+echo 'alias src="source env/bin/activate"' >> ~/.profile 
+source ~/.profile 
+
+#install venv and requirements
+sudo apt install python3.8 python3.8-venv
+python3.8 -m venv env
+source env/bin/activate
+pip3.8 install -r requirements.txt
+
+#help text
+echo "just run 'src' whenever you enter the directory now!"
