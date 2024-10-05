99eaac58cec2779ebbae4c3b6930cd9507e650d9
Drone #9813 <9813@hexcorp.net>
2024-10-05 13:52:23 +0200

    added hint about token

--- restart-ai-deploy-control.sh
+++ restart-ai-deploy-control.sh
@@ -7,4 +7,4 @@ git pull
 docker build --tag ai_deploy_control:latest .
 
 # start new image; copy this and fill in the bot token manually
-# docker run --name AIDeployControl --detach --restart always --env BOT_TOKEN=<> -p 5000:5000 ai_deploy_control:latest
+# docker run --name AIDeployControl --detach --restart always --env BOT_TOKEN=<INSERT_TOKEN> -p 5000:5000 ai_deploy_control:latest
