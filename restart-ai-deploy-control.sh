7c3c4b3ae9d888fd3ca5628b9ed70d7c4637ef69
Drone #9813 <9813@hexcorp.net>
2024-10-05 13:51:14 +0200

    added instructions for getting this to run

---
+++ restart-ai-deploy-control.sh
@@ -0,0 +1,10 @@
+# remove old deployment if exists
+docker stop AIDeployControl
+docker rm AIDeployControl
+
+# build new image
+git pull
+docker build --tag ai_deploy_control:latest .
+
+# start new image; copy this and fill in the bot token manually
+# docker run --name AIDeployControl --detach --restart always --env BOT_TOKEN=<> -p 5000:5000 ai_deploy_control:latest
